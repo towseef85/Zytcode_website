79acacbba61039ffade57218143105283674a98e
towseef85 <towseef85@gmail.com>
2021-10-23 14:46:29 +0300

    made services dynamic

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
@@ -14,40 +14,40 @@ import serviceSingle from './pages/serviceSingle';
 
 
 const ComponentWithLayout = (Component) => {
- 
-    return (props) => (
-      <Layout1>
-        <Component {...props} />
-      </Layout1>
-    );
-  };
-  
-  const ComponentWithLayout2 = (Component) => {
-    return (props) => (
-      <Layout2>
-        <Component {...props} />
-      </Layout2>
-    );
-  };
+
+  return (props) => (
+    <Layout1>
+      <Component {...props} />
+    </Layout1>
+  );
+};
+
+const ComponentWithLayout2 = (Component) => {
+  return (props) => (
+    <Layout2>
+      <Component {...props} />
+    </Layout2>
+  );
+};
 
 function Routes(props) {
   useEffect(() => {
     props.hideLoader();
   }, [])
-    return (
-        <BrowserRouter>
-     
-        <Switch>
-          <Route exact path="/" component={ComponentWithLayout(Home)} />
-          <Route exact path="/services" component={ComponentWithLayout2(Services)} />
-          <Route exact path="/projects" component={ComponentWithLayout2(Projects)} />
-          <Route exact path="/contact" component={ComponentWithLayout2(Contact)} />
-          <Route exact path="/about" component={ComponentWithLayout2(About)} />
-          <Route exact path="/projectSingle" component={ComponentWithLayout2(ProjectSingle)} />
-          <Route exact path="/serviceSingle" component={ComponentWithLayout2(serviceSingle)} />
-        </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+
+      <Switch>
+        <Route exact path="/" component={ComponentWithLayout(Home)} />
+        <Route exact path="/services" component={ComponentWithLayout2(Services)} />
+        <Route exact path="/projects" component={ComponentWithLayout2(Projects)} />
+        <Route exact path="/contact" component={ComponentWithLayout2(Contact)} />
+        <Route exact path="/about" component={ComponentWithLayout2(About)} />
+        <Route exact path="/projectSingle" component={ComponentWithLayout2(ProjectSingle)} />
+        <Route exact path="/serviceSingle/:id" component={ComponentWithLayout2(serviceSingle)} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default Routes
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,64 +1,34 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Sectitle from './Title/Sectitle';
 import { Link } from 'react-router-dom';
+import { SERVICES } from '../data/services';
 
 function Services() {
-    const integrationItem =[
-        {
-            "id": 100,
-            "iImage": "web.png",
-            "text": "Web Development"
-        },
-        {
-            "id": 101,
-            "iImage": "mobile.png",
-            "text": "Mobile Apps"
-        },
-        {
-            "id": 102,
-            "iImage": "nicereply-1.png",
-            "text": "Ecommerce"
-        },
-        {
-            "id": 103,
-            "iImage": "campfire.png",
-            "text": "ML & AI"
-        },
-        {
-            "id": 104,
-            "iImage": "webhooks.png",
-            "text": "Travel"
-        },
-        {
-            "id": 105,
-            "iImage": "briteverify.png",
-            "text": "Animation"
-        }
-    ]
+
     return (
         <section className="support_integration_area" id="services">
-                <div className="container">
-                    <Sectitle sClass="sec_title text-center mb_70" Title='Services' TitleP='The best path to application development and management is to start in the future – and stay there'/>
-                    <div className="row flex-row-reverse">
-                        <div className="col-lg-9 col-md-10 col-sm-12">
-                            <div className="row">
-                                {integrationItem.map(post =>(
-                                    <div className="col-lg-4 col-md-4 col-sm-6" key={post.id}>
-                                        <Link to="/serviceSingle" className="s_integration_item">
-                                            <img src={require ("../img/new-home/" + post.iImage)} alt=""/>
-                                            <h5>{post.text}</h5>
-                                        </Link>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-md-2 col-sm-12">
-                            <img className="integration_img" src={require ("../img/new-home/tree.png")} alt=""/>
+            <div className="container">
+                <Sectitle sClass="sec_title text-center mb_50" Title='Services' TitleP='The best path to application development and management is to start in the future – and stay there' />
+                <div className="row flex-row-reverse">
+                    <div className="col-lg-9 col-md-10 col-sm-12">
+                        <div className="row row-eq-height">
+                            {SERVICES.map(service => (
+                                <div className="col-lg-4 col-md-4 col-sm-6" key={service.id}>
+                                    <Link to={`/serviceSingle/${service.id}`} className="s_integration_item">
+                                        <img style={{ maxWidth: '50%' }} src={require("../img/new-home/" + service.img)} alt="" />
+                                        <h5>{service.title}</h5>
+                                    </Link>
+                                </div>
+                            ))}
                         </div>
                     </div>
+                    <div className="col-lg-3 col-md-2 col-sm-12">
+                        <img className="integration_img" src={require("../img/new-home/tree.png")} alt="" />
+                    </div>
                 </div>
-            </section>
+            </div>
+        </section>
     )
 }
 
